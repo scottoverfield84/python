2d1622df6dddc178f65e4f39f4d566b7fd31b86c
Scott Overfield <scottoverfield84@gmail.com>
2022-07-26 15:31:31 -0400

    Alarm.py

--- alarm.py
+++ alarm.py
@@ -1,5 +1,5 @@
 
-from datetime import datetime   
+from datetime import datetime
 from playsound import playsound
 alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
 alarm_hour=alarm_time[0:2]
